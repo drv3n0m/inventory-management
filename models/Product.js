97534ae92f22bc994a5bf48c55887221a4504a69
Asif Anjum <asfrabi15@gmail.com>
2022-10-02 12:26:19 +0600

    model created

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
 
 //schema design
 const productSchema = mongoose.Schema(
@@ -7,6 +8,7 @@ const productSchema = mongoose.Schema(
       type: String,
       required: [true, "Please provide a name for this product."],
       trim: true,
+      lowercase: true,
       unique: [true, "Name must be unique"],
       minLength: [3, "Name must be at least 3 characters."],
       maxLength: [100, "Name is too large"],
@@ -15,62 +17,50 @@ const productSchema = mongoose.Schema(
       type: String,
       required: true,
     },
-    price: {
-      type: Number,
-      required: true,
-      min: [0, "Price can't be negative"],
-    },
+
     unit: {
       type: String,
       required: true,
       enum: {
-        values: ["kg", "litre", "pcs"],
-        message: "unit value can't be {VALUE}, must be kg/litre/pcs",
+        values: ["kg", "litre", "pcs", "bag"],
+        message: "unit value can't be {VALUE}, must be kg/litre/pcs/bag",
       },
     },
-    quantity: {
-      type: Number,
-      required: true,
-      min: [0, "quantity cant be negative"],
-      validate: {
-        validator: (value) => {
-          const isInteger = Number.isInteger(value);
-          if (isInteger) {
-            return true;
-          } else {
+    imageURLs: [
+      {
+        type: String,
+        required: true,
+        validate: (value) => {
+          if (!Array.isArray(value)) {
             return false;
           }
+          let isValid = true;
+          value.forEach((url) => {
+            if (!validator.isURL(url)) {
+              isValid = false;
+            }
+          });
+          return isValid;
         },
+        message: "Please provide valid image urls",
       },
-      message: "Quantity must be an integer",
-    },
-    status: {
+    ],
+
+    category: {
       type: String,
       required: true,
-      enum: {
-        values: ["in-stock", "out-of-stock", "discontinued"],
-        message: "status can't be {VALUE}",
+    },
+    brand: {
+      name: {
+        type: String,
+        required: true,
+      },
+      id: {
+        type: ObjectId,
+        ref: "Brand",
+        required: true,
       },
     },
-    // createdAt: {
-    //   type: Date,
-    //   default: Date.now,
-    // },
-    // updatedAt: {
-    //   type: Date,
-    //   default: Date.now
-    // }
-    // supplier: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: "Supplier"
-    // },
-    // categories: [{
-    //   name: {
-    //     type: String,
-    //     required: true
-    //   },
-    //   _id: mongoose.Schema.Types.ObjectId
-    // }]
   },
   {
     timestamps: true,
